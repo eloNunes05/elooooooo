4a113734fc029a30e0822e5ce5e2a955c00fd725
eloisa nunes <149582104+eloNunes05@users.noreply.github.com>
2024-10-21 11:53:28 -0300

    elo

---
+++ perguntas.js
@@ -0,0 +1,15 @@
+criaCartão(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+criaCatao(
+    'Geografia',
+    'Qual a capital da frança?',
+    'A capital da frança é paris'
+)
+criaCartão(
+'Programação',
+'Oque é uma funçaõ?',
+'Uma função é um bloco de código que executa alguma tarefa'
+)
